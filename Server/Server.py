e187605e06bec5889239e681477b562d43cfddc4
tonypham90 <tuananh.gt@hotmail.com>
2021-09-20 23:52:48 +0700

    ok

--- Server/Server.py
+++ Server/Server.py
@@ -1,30 +1,32 @@
 import socket
+import sys
 
-HOST = '127.0.0.1'
-PORT = 8000
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(2)
-
-while True:
-    client, addr = s.accept()
-
+HOST = None               # Symbolic name meaning all available interfaces
+PORT = 50007              # Arbitrary non-privileged port
+s = None
+for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC,
+                              socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+    af, socktype, proto, canonname, sa = res
     try:
-        print('Connected by', addr)
-        while True:
-            data = client.recv(1024)
-            str_data = data.decode("utf8")
-            if str_data == "quit":
-                break
-            """if not data:
-                break
-            """
-            print("Client: " + str_data)
-
-            msg = input("Server: ")
-
-    finally:
-        client.close()
-
-    s.close()
+        s = socket.socket(af, socktype, proto)
+    except OSError as msg:
+        s = None
+        continue
+    try:
+        s.bind(sa)
+        s.listen(1)
+    except OSError as msg:
+        s.close()
+        s = None
+        continue
+    break
+if s is None:
+    print('could not open socket')
+    sys.exit(1)
+conn, addr = s.accept()
+with conn:
+    print('Connected by', addr)
+    while True:
+        data = conn.recv(1024)
+        if not data: break
+        conn.send(data)
